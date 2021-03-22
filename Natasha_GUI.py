fe4ee93d9571a910d3d2dd04028815424786e528
colonelul <claudiu9112@gmail.com>
2021-03-22 20:06:55 +0200

    bumbum

--- Natasha_GUI.py
+++ Natasha_GUI.py
@@ -1,7 +1,10 @@
+import time
+import threading
 import tkinter as tk
-from tkinter import Text, PhotoImage, Label
+from tkinter import * 
+from tkinter.ttk import *
 from due_communication import sendData
-#from numpad import numpadEnable 
+import serial
 
 root = tk.Tk()
 root.winfo_toplevel().title("NATASHA")
@@ -9,81 +12,253 @@ root.winfo_toplevel().title("NATASHA")
 #bg = PhotoImage(file = "/home.pi/Desktop/image.jpg") google
 
 root.attributes('-fullscreen', False) #fullscreen mode True
+ 
+# =============================================================================
+# canvas = tk.Canvas(root, height=1024, width=1280, bg="#263D42")
+# canvas.pack(fill=None, expand=False)
+# =============================================================================
 
-canvas = tk.Canvas(root, height=1024, width=1280, bg="#263D42")
-canvas.pack(fill=None, expand=False)
-
+root.geometry('1280x1024')
 
 filename = PhotoImage(file = "C:/Users/Work/Desktop/NATASHA-LCD/aa.png")
 background_label = Label(root, image=filename)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-# ~~~~~~~Button DECLARATION~~~~~~~
+serial_object = None
 
-startMotor = tk.Button(root, text="START", padx=60, pady=5, fg="white", bg="#263D42")
-stopMotor = tk.Button(root, text="STOP", padx=60, pady=5, fg="white", bg="#263D42")
-MotorStepPlus = tk.Button(root, text="+", padx=30, pady=5, fg="white", bg="#263D42")
-MotorStepMinus = tk.Button(root, text="-", padx=30, pady=5, fg="white", bg="#263D42")
+def connect():
 
-startMotor.place(x=650, y= 150)
-stopMotor.place(x=1000, y= 150)
-MotorStepPlus.place(x=690, y= 250)
-MotorStepMinus.place(x=1040, y= 250)
+    locations=['/dev/ttyACM0','/dev/ttyACM1','/dev/ttyACM2','/dev/ttyACM3']
 
-# ~~~~~~~Temperature OUT-DECLARATION~~~~~~~
+    for device in locations:
+        try:
+            print("Trying..." + device)
+            serial_object = serial.Serial(device, 9600)
+            break
+        except:
+            print("Failed to connect on" + device)
+    
+    t1 = threading.Thread(target = get_data)
+    t1.daemon = True
+    t1.start()
 
-temp_out_1 = Text(root, height=2, width=6)
-temp_out_1.place(x=588,y=810)
-
-temp_out_2 = Text(root, height=2, width=6)
-temp_out_2.place(x=520,y=810)
-
-temp_out_3 = Text(root, height=2, width=6)
-temp_out_3.place(x=454,y=810)
-
-temp_out_4 = Text(root, height=2, width=6)
-temp_out_4.place(x=386,y=810)
-
-temp_out_5 = Text(root, height=2, width=6)
-temp_out_5.place(x=320,y=810)
-
-temp_out_6 = Text(root, height=2, width=6)
-temp_out_6.place(x=253,y=810)
-
-temp_out_7 = Text(root, height=2, width=6)
-temp_out_7.place(x=187,y=810)
-
-temp_out_8 = Text(root, height=2, width=6)
-temp_out_8.place(x=120,y=810)
-
-temp_out_9 = Text(root, height=2, width=6)
-temp_out_9.place(x=52,y=810)
+def update_gui():
+    global filter_data
+    global update_period
+    
+    new = time.time()
 
 
-motor_rpm = Text(root, height=3, width=13)
-motor_rpm.place(x=843, y=820)
+def get_data():
+    global serial_object
+    global filter_data
 
-def temp_val_out(input_val, value):
-    inputValue = input_val.get("1.0","end-1c")
-    print(inputValue)
-    input_val.delete('1.0', 'end')
-    print(value)
-    send = value + ' ' + inputValue
-    sendData(send)
+    while(1):   
+        try:
+            serial_data = serial_object.readline().strip('\n').strip('\r')
+            
+            filter_data = serial_data.split(',')
+            print(filter_data)
+        
+        except TypeError:
+            pass
 
-def amp_info_out():
-    motor_rpm.config(text = "AMP:")
+def send():
+
+    send_data = temp_out_1.get()
+    
+    if not send_data:
+        print("Sent Nothing")
+    
+    serial_object.write(send_data)
+
+def keyboard(temp):
+    buttons = ['7', '8', '9',
+               '4', '5', '6',
+               '1', '2', '3',
+               '0', '.', 'Del',
+               'Enter']
+    
+    varRow = 2
+    varColumn = 0
+    for button in buttons:
+        command = lambda x = button: select(x, temp)
+        if button != 'Enter':
+            tk.Button(root, text = button, width = 5, bg="#000000", fg="#ffffff", activebackground="#ffffff",
+                   activeforeground="#000000", relief = 'raised', padx=4, pady=4, bd=4, command=command).grid(row = varRow, column = varColumn)
+        else:
+             tk.Button(root, text = button, width = 15, bg="#000000", fg="#ffffff", activebackground="#ffffff",
+                   activeforeground="#000000", relief = 'raised', padx=4, pady=4, bd=4, command=command).grid(row = 6, column = 1)
+        
+        varColumn+=1
+        if varColumn > 2 and varRow == 2:
+            varColumn = 0
+            varRow+=1
+        if varColumn > 2 and varRow == 3:
+            varColumn = 0
+            varRow+=1
+        if varColumn > 2 and varRow == 4:
+            varColumn = 0
+            varRow+=1
+        if varColumn > 2 and varRow == 5:
+            varColumn = 0
+            varRow+=1
+
+def select(x, value):
+    temp_out_1.insert(tk.END, x)
+
+def callback(sv):
+    print(sv.get())
+
+def sys_out(event):
+    sys.exit()
+
+if __name__ == "__main__":
+    
+    connect()
+    
+    t2 = threading.Thread(target = update_gui)
+    t2.daemon = True
+    t2.start()
+    
+    # ~~~~~~~Button DECLARATION~~~~~~~
+
+    startMotor = tk.Button(root, text="START", padx=85, pady=20, fg="white", bg="green")
+    MotorStepPlus = tk.Button(root, text="+", padx=42, pady=20, fg="white", bg="blue")
+    MotorStepMinus = tk.Button(root, text="-", padx=42, pady=20, fg="white", bg="blue")
+
+    
+    startMotor.place(x=1035, y= 695)
+    MotorStepPlus.place(x=910, y= 695)
+    MotorStepMinus.place(x=800, y= 695)
+    
+    sv = StringVar()
+    sv.trace("w", lambda name, index, mode, sv=sv: callback(sv))
+    
+    
+
+    # ~~~~~~~Temperature OUT-DECLARATION~~~~~~~
+
+    temp_out_1 = Entry(width = 7, textvariable=sv)
+    temp_out_2 = Entry(width = 7, textvariable=sv)
+    temp_out_3 = Entry(width = 7, textvariable=sv)
+    temp_out_4 = Entry(width = 7, textvariable=sv)
+    temp_out_5 = Entry(width = 7, textvariable=sv)
+    temp_out_6 = Entry(width = 7, textvariable=sv)
+    temp_out_7 = Entry(width = 7, textvariable=sv)
+    temp_out_8 = Entry(width = 7, textvariable=sv)
+    temp_out_9 = Entry(width = 7, textvariable=sv)
+    temp_out_11 = Entry(width = 7, textvariable=sv)
+    temp_out_12 = Entry(width = 7, textvariable=sv)
 
 
-temp_out_1.bind('<Return>', lambda event: temp_val_out(input_val=temp_out_1, value='t1'))
-temp_out_2.bind('<Return>', lambda event: temp_val_out(input_val=temp_out_2, value='t2'))
-temp_out_3.bind('<Return>', lambda event: temp_val_out(input_val=temp_out_3, value='t3'))
-temp_out_4.bind('<Return>', lambda event: temp_val_out(input_val=temp_out_4, value='t4'))
-temp_out_5.bind('<Return>', lambda event: temp_val_out(input_val=temp_out_5, value='t5'))
-temp_out_6.bind('<Return>', lambda event: temp_val_out(input_val=temp_out_6, value='t6'))
-temp_out_7.bind('<Return>', lambda event: temp_val_out(input_val=temp_out_7, value='t7'))
-temp_out_8.bind('<Return>', lambda event: temp_val_out(input_val=temp_out_8, value='t8'))
-temp_out_9.bind('<Return>', lambda event: temp_val_out(input_val=temp_out_9, value='t9'))
+    # ~~~~~~~Temperature IN-DECLARATION~~~~~~~
+    
+    temp_int_1 = Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 50, max = 150)   
+    temp_int_2 = Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 50, max = 150)   
+    temp_int_3 = Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 50, max = 255)   
+    temp_int_4 = Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 50, max = 255)   
+    temp_int_5 = Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 50, max = 255)   
+    temp_int_6 = Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 50, max = 255)   
+    temp_int_7 = Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 50, max = 255)   
+    temp_int_8 = Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 50, max = 255)   
+    temp_int_9 = Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 50, max = 255)   
+    temp_int_10 = Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 50, max = 255)   
+    temp_int_11 = Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 50, max = 255)   
+    temp_int_12 = Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 50, max = 255)   
+    temp_int_13 = Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 50, max = 255)   
+    temp_int_14 = Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 50, max = 255)   
+    temp_int_15 = Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 50, max = 255)   
+    temp_int_16 = Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 50, max = 255)   
+    temp_int_17 = Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 50, max = 255)   
+    temp_int_18 = Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 50, max = 255)   
+    temp_int_19 = Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 50, max = 255)   
+    temp_int_20 = Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 50, max = 255)   
+    
+    # ~~~~~~~Temperature IN-DECLARATION~~~~~~~
+    
+    amp_info = Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 50, max = 255)   
 
+    
+    # ~~~~~~~RPM IN-DECLARATION~~~~~~~
+    
+    rpm_1 = Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 50, max = 150)   
+    rpm_2 = Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 50, max = 150)   
+    rpm_3 = Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 50, max = 150)   
+    rpm_4 = Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 50, max = 150)   
+    rpm_5 = Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 50, max = 150)   
+    rpm_6 = Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 50, max = 150)   
+    rpm_7 = Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 50, max = 150)   
+    rpm_8 = Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 50, max = 150)   
+    rpm_9 = Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 50, max = 150)
+    rpm_motor_set = Entry(width = 7) 
+    flow = Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 100, max = 150)   
+    
+
+    # ~~~~~~~Temperature OUT-LOCATION~~~~~~~
+
+    temp_out_1.place(x=588,y=810)
+    temp_out_2.place(x=520,y=810)
+    temp_out_3.place(x=454,y=810)
+    temp_out_4.place(x=386,y=810)
+    temp_out_5.place(x=320,y=810)
+    temp_out_6.place(x=253,y=810)
+    temp_out_7.place(x=187,y=810)
+    temp_out_8.place(x=120,y=810)
+    temp_out_9.place(x=52,y=810)
+    temp_out_11.place(x=350,y=720)
+    temp_out_12.place(x=40,y=720)
+
+    # ~~~~~~~Temperature IN-LOCATION~~~~~~~
+
+    temp_int_1.place(x = 588, y = 850)
+    temp_int_2.place(x = 520, y = 850)
+    temp_int_3.place(x = 454, y = 850)
+    temp_int_4.place(x = 386, y = 850)
+    temp_int_5.place(x = 320, y = 850)
+    temp_int_6.place(x = 253, y = 850)
+    temp_int_7.place(x = 187, y = 850)
+    temp_int_8.place(x = 120, y = 850)
+    temp_int_9.place(x = 52, y = 850)
+    temp_int_10.place(x = 685, y = 830)
+    temp_int_11.place(x = 350, y = 760)
+    temp_int_12.place(x = 40, y = 760)
+    temp_int_13.place(x = 100, y = 100)
+    temp_int_14.place(x = 100, y = 100)
+    temp_int_15.place(x = 100, y = 100)
+    temp_int_16.place(x = 100, y = 100)
+    temp_int_17.place(x = 100, y = 100)
+    temp_int_18.place(x = 100, y = 100)
+    temp_int_19.place(x = 100, y = 100)
+    temp_int_20.place(x = 100, y = 100)
+    
+    # ~~~~~~~RPM IN-LOCATION~~~~~~~
+    
+    rpm_1.place(x=588,y=920)
+    rpm_2.place(x=520,y=920)
+    rpm_3.place(x=454,y=920)
+    rpm_4.place(x=386,y=920)
+    rpm_5.place(x=320,y=920)
+    rpm_6.place(x=253,y=920)
+    rpm_7.place(x=187,y=920)
+    rpm_8.place(x=120,y=920)
+    rpm_9.place(x=52,y=920)
+    rpm_motor_set.place(x=850,y=800)
+    flow.place(x=850,y=850)
+    
+    # ~~~~~~~AMP IN-LOCATION~~~~~~~
+    
+    amp_info.place(x=1100,y=830)
+    
+    temp_out_1.bind("<1>", lambda event: keyboard(temp = 'temp_out_1'))
+    temp_out_2.bind("<1>", keyboard)
+    temp_out_3.bind("<1>", keyboard)
+    temp_out_4.bind("<1>", keyboard)
+    temp_out_5.bind("<1>", keyboard)
+    temp_out_6.bind("<1>", keyboard)
+    temp_out_7.bind("<1>", keyboard)
+    temp_out_8.bind("<1>", keyboard)
+    temp_out_9.bind("<1>", keyboard)
+    root.bind("Key", sys_out)
 
 root.mainloop()
--- numpad.py
+++ numpad.py
@@ -1,197 +1,147 @@
-from tkinter import *
 
+# import modules
+try:
+    import Tkinter
+except:
+    import tkinter as Tkinter
 
-class _PopupKeyboard(Toplevel):
-    '''A Toplevel instance that displays a keyboard that is attached to
-    another widget. Only the Entry widget has a subclass in this version.
-    '''
+keys =[ 
+[
+ [
+  # Layout Name
+  ("Function_Keys"),
+
+  # Layout Frame Pack arguments
+  ({'side':'top','expand':'yes','fill':'both'}),
+  [
+   # list of Keys
+   ('esc'," ", 'F1', 'F2','F3','F4',"",'F5','F6','F7','F8',"",'F9','F10','F11','F12')
+  ]
+ ],
+
+ [
+  ("Character_Keys"),
+  ({'side':'top','expand':'yes','fill':'both'}),
+  [
+   ('~\n`','!\n1','@\n2','#\n3','$\n4','%\n5','^\n6','&\n7','*\n8','(\n9',')\n0','_\n-','+\n=','|\n\\','backspace'),
+   ('tab','q','w','e','r','t','y','u','i','o','p','{\n[','}\n]','   '),
+   ('capslock','a','s','d','f','g','h','j','k','l',':\n;',"\"\n'","enter"),
+   ("shift",'z','x','c','v','b','n','m','<\n,','>\n.','?\n/',"shift"),
+   ("ctrl", "[+]",'alt','\t\tspace\t\t','alt','[+]','[=]','ctrl')
+  ]
+ ]
+],
+[
+ [
+  ("System_Keys"),
+  ({'side':'top','expand':'yes','fill':'both'}),
+  [
+   (
+    "print\nscreen\nsys",
+    "scroll\nlock",
+    "pause\nbreak"
+   )
+  ]
+ ],
+ [
+  ("Editing_Keys"),
+  ({'side':'top','expand':'yes','fill':'both'}),
+  [
+   (
+    "insert",
+    "home",
+    "page\nup"
+    ),
+   ( "delete",
+    "end",
+    "page\ndown"
+    ),
+  ]
+ ],
+
+ [
+  ("Navigation_Keys"),
+  ({'side':'top','expand':'yes','fill':'both'}),
+  [
+   (
+    "up",
+    ),
+   ( "right",
+    "down",
+    "left"
+    ),
+  ]
+ ],
+
+],
+[
+
+ [
+  ("Numeric_Keys"),
+  ({'side':'top','expand':'yes','fill':'both'}),
+  [
+   ("num\nlock","/","*","-"),
+   ("7","8","9","+"),
+   ("4","5","6"," "),
+   ("0","1","2","3"),
+   ("0",".","enter")
+  ]
+ ],
+
+]
+
+]
+
+##  Frame Class
+class Keyboard(Tkinter.Frame):
+    def __init__(self, *args, **kwargs):
+     Tkinter.Frame.__init__(self, *args, **kwargs)
     
-    def __init__(self, parent, attach, x, y, keycolor, keysize=5):
-        Toplevel.__init__(self, takefocus=0)
-        
-        self.overrideredirect(True)
-        self.attributes('-alpha',0.85)
-
-        self.parent = parent
-        self.attach = attach
-        self.keysize = keysize
-        self.keycolor = keycolor
-        self.x = x
-        self.y = y
-
-        self.row1 = Frame(self)
-        self.row2 = Frame(self)
-        self.row3 = Frame(self)
-        self.row4 = Frame(self)
-
-        self.row1.grid(row=1)
-        self.row2.grid(row=2)
-        self.row3.grid(row=3)
-        self.row4.grid(row=4)
-        
-        self._init_keys()
-
-        # destroy _PopupKeyboard on keyboard interrupt
-        self.bind('<Key>', lambda e: self._destroy_popup())
-
-        # resize to fit keys
-        self.update_idletasks()
-        self.geometry('{}x{}+{}+{}'.format(self.winfo_width(),
-                                           self.winfo_height(),
-                                           self.x,self.y))
-        
-    def _init_keys(self):
-        self.alpha = {
-            'row1' : ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p','/'],
-            'row2' : ['<<<','a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',',','>>>'],
-            'row3' : ['shift','z', 'x', 'c', 'v', 'b', 'n', 'm','.','?','[1,2,3]'],
-            'row4' : ['@','#','%','*','[ space ]','+','-','=']
-            }
-        
-        for row in self.alpha.iterkeys(): # iterate over dictionary of rows
-            if row == 'row1':             # TO-DO: re-write this method
-                i = 1                     # for readability and functionality
-                for k in self.alpha[row]:
-                    Button(self.row1,
-                           text=k,
-                           width=self.keysize,
-                           bg=self.keycolor,
-                           command=lambda k=k: self._attach_key_press(k)).grid(row=0,column=i)
-                    i += 1
-            elif row == 'row2':
-                i = 2
-                for k in self.alpha[row]:
-                    Button(self.row2,
-                           text=k,
-                           width=self.keysize,
-                           bg=self.keycolor,
-                           command=lambda k=k: self._attach_key_press(k)).grid(row=0,column=i)
-                    i += 1
-            elif row == 'row3':
-                i = 2
-                for k in self.alpha[row]:
-                    Button(self.row3,
-                           text=k,
-                           width=self.keysize,
-                           bg=self.keycolor,
-                           command=lambda k=k: self._attach_key_press(k)).grid(row=0,column=i)
-                    i += 1
+     # Function For Creating Buttons
+     self.create_frames_and_buttons()
+    
+    # Function For Extracting Data From KeyBoard Table
+    # and then provide us a well looking
+    # keyboard gui
+    def create_frames_and_buttons(self):
+     # take section one by one
+        for key_section in keys:
+         # create Sperate Frame For Every Section
+         store_section = Tkinter.Frame(self)
+         store_section.pack(side='left',expand='yes',fill='both',padx=10,pady=10,ipadx=10,ipady=10)
+         
+         for layer_name, layer_properties, layer_keys in key_section:
+          store_layer = Tkinter.LabelFrame(store_section)#, text=layer_name)
+          #store_layer.pack(side='top',expand='yes',fill='both')
+          store_layer.pack(layer_properties)
+          for key_bunch in layer_keys:
+           store_key_frame = Tkinter.Frame(store_layer)
+           store_key_frame.pack(side='top',expand='yes',fill='both')
+           for k in key_bunch:
+            k=k.capitalize()
+            if len(k)<=3:
+             store_button = Tkinter.Button(store_key_frame, text=k, width=2, height=2)
             else:
-                i = 3
-                for k in self.alpha[row]:
-                    if k == '[ space ]':
-                        Button(self.row4,
-                               text=k,
-                               width=self.keysize * 3,
-                               bg=self.keycolor,
-                               command=lambda k=k: self._attach_key_press(k)).grid(row=0,column=i)
-                    else:
-                        Button(self.row4,
-                               text=k,
-                               width=self.keysize,
-                               bg=self.keycolor,
-                               command=lambda k=k: self._attach_key_press(k)).grid(row=0,column=i)
-                    i += 1
+             store_button = Tkinter.Button(store_key_frame, text=k.center(5,' '), height=2)
+            if " " in k:
+             store_button['state']='disable'
+            #flat, groove, raised, ridge, solid, or sunken
+            store_button['relief']="sunken"
+            store_button['bg']="powderblue"
+            store_button['command']=lambda q=k: self.button_command(q)
+            store_button.pack(side='left',fill='both',expand='yes')
+        return
 
-    def _destroy_popup(self):
-        self.destroy()
+  # Function For Detecting Pressed Keyword.
+    def button_command(self, event):
+        print(event)
+        return
 
-    def _attach_key_press(self, k):
-        if k == '>>>':
-            self.attach.tk_focusNext().focus_set()
-            self.destroy()
-        elif k == '<<<':
-            self.attach.tk_focusPrev().focus_set()
-            self.destroy()
-        elif k == '[1,2,3]':
-            pass
-        elif k == '[ space ]':
-            self.attach.insert(END, ' ')
-        else:
-            self.attach.insert(END, k)
+# Creating Main Window
+def main():
+ root = Tkinter.Tk(className=" Python Virtual KeyBoard")
+ Keyboard(root).pack()
+ root.mainloop()
+ return
 
-'''
-TO-DO: Implement Number Pad
-class _PopupNumpad(Toplevel):
-    def __init__(self, x, y, keycolor='gray', keysize=5):
-        Toplevel.__init__(self, takefocus=0)
-        
-        self.overrideredirect(True)
-        self.attributes('-alpha',0.85)
-        self.numframe = Frame(self)
-        self.numframe.grid(row=1, column=1)
-        self.__init_nums()
-        self.update_idletasks()
-        self.geometry('{}x{}+{}+{}'.format(self.winfo_width(),
-                                           self.winfo_height(),
-                                           self.x,self.y))
-    def __init_nums(self):
-        i=0
-        for num in ['7','4','1','8','5','2','9','6','3']:
-            print num
-            Button(self.numframe,
-                   text=num,
-                   width=int(self.keysize/2),
-                   bg=self.keycolor,
-                   command=lambda num=num: self.__attach_key_press(num)).grid(row=i%3, column=i/3)
-            i+=1
-'''
-
-class KeyboardEntry(Frame):
-    '''An extension/subclass of the Tkinter Entry widget, capable
-    of accepting all existing args, plus a keysize and keycolor option.
-    Will pop up an instance of _PopupKeyboard when focus moves into
-    the widget
-    Usage:
-    KeyboardEntry(parent, keysize=6, keycolor='white').pack()
-    '''
-    
-    def __init__(self, parent, keysize=5, keycolor='gray', *args, **kwargs):
-        Frame.__init__(self, parent)
-        self.parent = parent
-        
-        self.entry = Entry(self, *args, **kwargs)
-        self.entry.pack()
-
-        self.keysize = keysize
-        self.keycolor = keycolor
-        
-        self.state = 'idle'
-        
-        self.entry.bind('<FocusIn>', lambda e: self._check_state('focusin'))
-        self.entry.bind('<FocusOut>', lambda e: self._check_state('focusout'))
-        self.entry.bind('<Key>', lambda e: self._check_state('keypress'))
-
-    def _check_state(self, event):
-        '''finite state machine'''
-        if self.state == 'idle':
-            if event == 'focusin':
-                self._call_popup()
-                self.state = 'virtualkeyboard'
-        elif self.state == 'virtualkeyboard':
-            if event == 'focusin':
-                self._destroy_popup()
-                self.state = 'typing'
-            elif event == 'keypress':
-                self._destroy_popup()
-                self.state = 'typing'
-        elif self.state == 'typing':
-            if event == 'focusout':
-                self.state = 'idle'
-        
-    def _call_popup(self):
-        self.kb = _PopupKeyboard(attach=self.entry,
-                                 parent=self.parent,
-                                 x=self.entry.winfo_rootx(),
-                                 y=self.entry.winfo_rooty() + self.entry.winfo_reqheight(),
-                                 keysize=self.keysize,
-                                 keycolor=self.keycolor)
-
-    def _destroy_popup(self):
-        self.kb._destroy_popup()
-
-def test():  
-    root = Tk()
-    KeyboardEntry(root, keysize=6, keycolor='white').pack()
-    KeyboardEntry(root).pack()
-    root.mainloop()
+main()
